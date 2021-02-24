c349c08778d22738928a95acc0454941c393e665
Andrei Silviu Dragnea <andreisilviudragnea@gmail.com>
2021-02-24 22:14:40 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("org.springframework.boot") version "2.4.2"
-  id("io.spring.dependency-management") version "1.0.10.RELEASE"
+  id("org.springframework.boot") version "2.4.3"
+  id("io.spring.dependency-management") version "1.0.11.RELEASE"
   kotlin("jvm") version "1.4.30"
   kotlin("plugin.spring") version "1.4.30"
 }
